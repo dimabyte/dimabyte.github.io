1d00abb37b4104711c3174cf4d4e6fdd2632110d
Dima <dimabytes@gmail.com>
2018-10-01 23:38:21 +0300

    Попытка вернуть задержку

--- scripts/script.js
+++ scripts/script.js
@@ -58,6 +58,7 @@ $(function() {
 
 
         $('.preloader').css('height', window.innerHeight);
+        $('.preloader').css('transition', '1s');
         check_little();
     $(window).on('resize', check_little);
 
