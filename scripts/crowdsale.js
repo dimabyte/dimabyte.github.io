80348c6106396b81821b6802e87b9e378d483ecb
Dima <dimabytes@gmail.com>
2018-10-28 19:15:02 +0300

    Главная. canvas 1.4.8(Попытка фикса мобильных)

--- scripts/crowdsale.js
+++ scripts/crowdsale.js
@@ -19,7 +19,7 @@ function draw_all(dots = 90) {
             a.push(new Drawer(
                 two_rand(sheet.width - 3, 1), //coordinate by x
                 two_rand(sheet.height - 3, 1), //coordinate by y
-                4, 4, 'black', //width, height, color
+                2, 2, 'black', //width, height, color
                 none_zero(2), //x vector
                 none_zero(2), //y vector
             ));
