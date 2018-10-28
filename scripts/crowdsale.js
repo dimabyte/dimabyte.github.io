c2b530c0ab4e50e3c15950ed10f0fb97aaeeadad
Dima <dimabytes@gmail.com>
2018-10-28 22:05:24 +0300

    Главная. canvas 1.6.1(Попытка фикса мобильных)

--- scripts/crowdsale.js
+++ scripts/crowdsale.js
@@ -67,7 +67,7 @@ function draw_all(dots = 90) {
                     context.lineWidth = 1;
                     let line_opacity = (100 - Math.pow(Math.pow(a[k].x - a[h].x, 2) + Math.pow(a[k].y - a[h].y, 2), 0.5)) / 100;
                     //opacity of line turn on the distance
-                    context.strokeStyle = "hsla(210, 27%, 83%," + line_opacity + ")";
+                    context.strokeStyle = "hsla(208, 32%, 90%," + line_opacity + ")";
                     context.stroke();
                 }
                 k -= 1
